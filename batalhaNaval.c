77d9c1dc6dacb85fa31b1daf7488c224a2a8615a
Leonardo Ly-On! <skleozin@gmail.com>
2025-04-02 19:22:53 +0000

    Nível Aventureiro (Concluído) Posicionando os navios em diagonal.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,69 @@
 #define MAR 0
 #define TAMANHONAVIO 3
+#define NAVIO 3
 
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+//Função pra verificar se os navios não se sobrepõem.
+int verificarPosicao(int tabuleiro[10][10], int linha, int coluna, char direcao){
+    for (int i = 0; i < TAMANHONAVIO; i++){
+        if (linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10 || tabuleiro[linha][coluna] == NAVIO){
+            return 0;
+        }
 
-
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    int tabuleiro [10][10];
-    char letras [] = "ABCDEFGHIJ";
-
-    int navioHorizontal = 5;
-    int navioVertical = 2;
-
-    // Criando a matriz do tabuleiro.
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
-            tabuleiro[i][j] = MAR;
+        //Ajusta as coordenadas para fazer a verificação completa.
+        if (direcao == 'H' || direcao == 'h'){
+            coluna++;
+        } else if (direcao == 'V' || direcao == 'v'){
+            linha++;
+        } else if (direcao == 'C' || direcao == 'c'){
+            linha--;
+            coluna++;
+        } else if (direcao == 'D' || direcao == 'd'){
+            linha++;
+            coluna++;
         }
     }
-    //Posicionando Navio na horizontal.
-    for (int c = 0; c < TAMANHONAVIO; c++){
-        tabuleiro[navioHorizontal][c] = 3;
-    }
 
-    // Posicionando navio na vertical.
-    for (int d = 0; d < TAMANHONAVIO; d++){
-        tabuleiro[d][navioVertical] = 3;
-    }
+    return 1;
+}
 
+
+// Procedimento para poder posicionar os navioss.
+void posicionarNavio(int tabuleiro[10][10], int linha, int coluna, char direcao){
+    if (verificarPosicao(tabuleiro, linha, coluna, direcao)){
+        for (int i = 0; i < TAMANHONAVIO; i++){
+            tabuleiro[linha][coluna] = NAVIO;
+            
+            //Verifica a direçao.
+            if (direcao == 'H' || direcao == 'h'){
+                coluna++;
+            } else if (direcao == 'V' || direcao == 'v'){
+                linha++;
+            } else if (direcao == 'C' || direcao == 'c'){
+                linha--;
+                coluna++;
+            } else if (direcao == 'D' || direcao == 'd'){
+                linha++;
+                coluna++;
+            } else{
+                printf("Direção inválida!");
+            }
+        }
+    } else{
+        printf("Posição já preenchida ou inexistente! Escolha outra!\n");
+    }
+}
+
+int main() {
+
+    int tabuleiro [10][10]={0};
+    char letras [] = "ABCDEFGHIJ";
+
+    posicionarNavio (tabuleiro, 1, 2, 'H');
+    posicionarNavio (tabuleiro, 4, 0, 'V');
+    posicionarNavio (tabuleiro, 3, 2, 'D');
+    posicionarNavio (tabuleiro, 8, 6, 'c');
 
     // Exibindo o tabuleiro com os navios em suas posições
     printf("   1 2 3 4 5 6 7 8 9 10\n");
@@ -47,35 +74,6 @@ int main() {
         }
         printf("\n");
     }
-
-
-    
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
